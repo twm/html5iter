51c750cc2ff2758ed960ddfaec68b85bbcadeb51
Tom Most <twm@freecog.net>
2020-07-02 22:32:08 -0700

    test_parser2 - 6

--- html5lib/tests/test_parser2.py
+++ html5lib/tests/test_parser2.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import, division, unicode_literals
 
-from six import PY2, text_type
-
 import io
 
 from . import support  # noqa
@@ -74,11 +72,6 @@ def test_debug_log():
                 ('dataState', 'InBodyPhase', 'InBodyPhase', 'processCharacters', {'type': 'Characters'}),
                 ('dataState', 'InBodyPhase', 'InBodyPhase', 'processEndTag', {'name': 'p', 'type': 'EndTag'}),
                 ('dataState', 'InBodyPhase', 'InBodyPhase', 'processCharacters', {'type': 'Characters'})]
-
-    if PY2:
-        for i, log in enumerate(expected):
-            log = [x.encode("ascii") if isinstance(x, text_type) else x for x in log]
-            expected[i] = tuple(log)
 
     assert parser.log == expected
 
