ea36a6d9cf6be3be3e85c626a67a6e1d97e088e3
Tom Most <twm@freecog.net>
2020-07-02 22:26:42 -0700

    test_treewalkers - 6

--- html5lib/tests/test_treewalkers.py
+++ html5lib/tests/test_treewalkers.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, division, unicode_literals
 import itertools
 import sys
 
-from six import unichr, text_type
 import pytest
 
 try:
@@ -151,7 +150,7 @@ def test_maintain_attribute_order(treeName):
         pytest.skip("Treebuilder not loaded")
 
     # generate loads to maximize the chance a hash-based mutation will occur
-    attrs = [(unichr(x), text_type(i)) for i, x in enumerate(range(ord('a'), ord('z')))]
+    attrs = [(chr(x), str(i)) for i, x in enumerate(range(ord('a'), ord('z')))]
     data = "<span " + " ".join("%s='%s'" % (x, i) for x, i in attrs) + ">"
 
     parser = html5parser.HTMLParser(tree=treeAPIs["builder"])
